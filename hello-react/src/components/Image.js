4eb8cf94d5698ae74ff0637ef55f75dbb1239a59
ikhwands <ikhwands@protonmail.ch>
2018-12-04 16:47:10 +0700

    Props pada Class Component

--- hello-react/src/components/Image.js
+++ hello-react/src/components/Image.js
@@ -4,9 +4,9 @@ class Image extends Component {
   render(){
     return(
       <img
-      src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/food1.jpg"
+      src={this.props.linkgambar}
       alt="gambar-makanan"
-      width="500"
+      width={this.props.lebar}
       />
     );
   }
--- hello-react/src/components/List.js
+++ hello-react/src/components/List.js
@@ -5,10 +5,17 @@ class List extends Component {
   render() {
     return(
       <ol>
-        <Image />
+        <Image linkgambar="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/food1.jpg"
+        lebar="500" />
         <li>Nasi Padang</li>
+        <Image linkgambar="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/nasipadang.jpg"
+        lebar="400" />
         <li>Sate</li>
+        <Image linkgambar="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+        lebar="300" />
         <li>Soto Lamongan</li>
+        <Image linkgambar="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sotolamongan.png"
+        lebar="200" />
       </ol>
     );
   }
