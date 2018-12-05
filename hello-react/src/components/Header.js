4e2728ddc1f11062d027a542f8431aac1b0710f4
ikhwands <ikhwands@protonmail.ch>
2018-12-05 08:46:35 +0700

    Penulisan State

--- hello-react/src/components/Header.js
+++ hello-react/src/components/Header.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      daftar: "Daftar Makanan Nusantara"
+    };
+  }
   render() {
     return(
       <div>
         <h2>Makanan khas Indonesia</h2>
+        <p>{this.state.daftar}</p>
       </div>
     );
   }
