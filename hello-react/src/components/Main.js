9ff7a77a13aa1cd2a358d605af2b588873bd0c9a
ikhwands <ikhwands@protonmail.ch>
2019-01-15 21:18:44 +0700

    Multiple State (clone to laptop)

--- hello-react/src/components/Main.js
+++ hello-react/src/components/Main.js
@@ -4,19 +4,26 @@ class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
-      title : "Menu Makanan"
+      title : "Menu Makanan",
+      title2 : "Menu Minuman"
     };
     this.rubahData = this.rubahData.bind(this);
   }
 
   rubahData(){
-    this.setState({title: "Pilih Makanan"});
+    this.setState((state, props) => {
+      return {
+        title: state.title2,
+        title2: state.title
+      };
+    });
   }
 
   render() {
     return(
       <div>
         <h3>{this.state.title}</h3>
+        <h2>{this.state.title2}</h2>
         <button onClick={this.rubahData}>Rubah Data</button>
       </div>
     )
